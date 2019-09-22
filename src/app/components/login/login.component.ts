ef6744a9177777d7683b7834c8fc30e3d8eaebda
kevin <kevin-1134@live.com>
2019-09-22 16:31:38 -0500

    maquetar formulario login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
+import {  UserService } from '../../services/user.service';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers : [UserService]
 })
 export class LoginComponent implements OnInit {
   public pagetitle : string;
-  constructor() {
-    this.pagetitle = "Identificate";
+  public user : User;
+  constructor(
+    private _userService : UserService
+  ) {
+    this.pagetitle = "Acceder al sistema";
+    this.user = new User(1,'', '', 'ROLE_USER', '','', '', '');
+
    }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+    
   }
 
 }
