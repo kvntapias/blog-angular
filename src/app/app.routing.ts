522005a782445cf8d1a6701b5cb4f29abc44d0e2
kevin <kevin-1134@live.com>
2019-09-22 12:15:52 -0500

    componentes home y error

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,16 @@ import { Routes, RouterModule } from "@angular/router";
 //importar componentes
 import{ LoginComponent } from './components/login/login.component';
 import{ RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 //DEFINIR RUTAS
 const appRoutes : Routes = [
-    {path : '', component : LoginComponent},
-    {path : 'inicio', component : LoginComponent},
+    {path : '', component : HomeComponent},
+    {path : 'inicio', component : HomeComponent},
     {path : 'login', component : LoginComponent},
     {path : 'register', component : RegisterComponent},
+    {path : '**', component : ErrorComponent},
 ];
 //EXPORTAR CONFIG
 export const AppRoutingProviders : any[] = [];
