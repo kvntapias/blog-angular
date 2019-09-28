a5e606c2ee305ee7020e7a9c39602fdcaa1e85c5
kevin <kevin-1134@live.com>
2019-09-28 09:18:36 -0500

    completar datos de usuario en form editar

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers : [UserService]
 })
 export class UserEditComponent implements OnInit {
 
   public pagetitle : string;
   public user : User;
+  public identity;
+  public token;
 
-  constructor() { 
+  constructor(
+    private _userService : UserService 
+  ) {
     this.pagetitle = 'Ajustes de usuario';
     this.user = new User(1,'', '', 'ROLE_USER', '','', '', '');
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.user = this.identity;
   }
 
   ngOnInit() {
