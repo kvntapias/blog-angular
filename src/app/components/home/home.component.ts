7415d27c837b8997babb06168958ba99a1f7339c
kevin <kevin-1134@live.com>
2019-09-29 16:26:25 -0500

    condicionar acciones segun usuario

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,29 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from "../../models/post";
 import { PostService } from "../../services/post.service";
 import { global } from "../../services/global";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [PostService]
+  providers: [PostService,UserService]
 })
 export class HomeComponent implements OnInit {
   public pagetitle : string;
   public url;
-  public posts : Array<Post>
+  public posts : Array<Post>;
+  public identity;
+  public token;
   constructor(
-    private _postService : PostService
+    private _postService : PostService, 
+    private _userService : UserService
   ) { 
     this.pagetitle = 'Inicio';
     this.url = global.url;
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   ngOnInit() {
     this.getPosts();
   }
-
-
+  
   getPosts(){
     this._postService.getPosts().subscribe(
       res => {
