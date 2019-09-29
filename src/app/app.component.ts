bef1b2a821908520997c664666ec2800feb0abae
kevin <kevin-1134@live.com>
2019-09-29 12:32:51 -0500

    listar categorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import {UserService} from './services/user.service';
-
+import { CategoryService } from "./services/category.service";
 import {global} from './services/global';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers : [UserService]
+  providers : [UserService, CategoryService]
 })
 export class AppComponent implements OnInit, DoCheck{
   title = 'Mi Blog';
   public identity;
   public token;
   public url;
+  public categories;
 
   constructor(
-    public _userService : UserService
+    private _userService : UserService,
+    private _categoryService : CategoryService
   ){
     this.loadUser();
     this.url = global.url;
@@ -25,16 +26,29 @@ export class AppComponent implements OnInit, DoCheck{
   }
 
   ngOnInit(){
+    this.getCategories();
   }
   
   ngDoCheck(){
       this.loadUser();
   }
 
-  
-
   loadUser(){
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
+  }
+
+  getCategories(){
+    this._categoryService.getCategories().subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.categories = response.categories;          
+        }
+      },
+      error => {
+        console.log(<any>error);
+        
+      }
+    );
   }
 }
--- src/app/components/category-new/category-new.component.ts
+++ src/app/components/category-new/category-new.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { UserService } from "../../services/user.service";
 import { Category } from "../../models/category";
+import {  CategoryService } from "../../services/category.service";
 
 @Component({
   selector: 'app-category-new',
   templateUrl: './category-new.component.html',
   styleUrls: ['./category-new.component.css'],
-  providers : [UserService]
+  providers : [UserService, CategoryService]
 })
 export class CategoryNewComponent implements OnInit {
 
@@ -15,11 +16,13 @@ export class CategoryNewComponent implements OnInit {
   public identity;
   public token;
   public category : Category;
+  public status : string;
 
   constructor(
     private _route : ActivatedRoute,
     private _router : Router,
-    private _userService : UserService
+    private _userService : UserService,
+    private _categoryService : CategoryService
   ) {
     this.pagetitle = 'Nueva categoria';
     this.identity = this._userService.getIdentity();
@@ -32,6 +35,22 @@ export class CategoryNewComponent implements OnInit {
 
   onSubmit(){
     console.log(this.category);
+    this._categoryService.create(this.token, this.category).subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.category = response.category;
+          this.status = 'success';
+          this._router.navigate(['/inicio']);
+        }else{
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+        
+      }
+    );
   }
 
 }
