54b05972f6e2f7a7c95a8169f4f47aa75681f4f0
kevin <kevin-1134@live.com>
2019-09-29 12:09:50 -0500

    form categorya

--- src/app/components/category-new/category-new.component.ts
+++ src/app/components/category-new/category-new.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { UserService } from "../../services/user.service";
+import { Category } from "../../models/category";
 
 @Component({
   selector: 'app-category-new',
   templateUrl: './category-new.component.html',
-  styleUrls: ['./category-new.component.css']
+  styleUrls: ['./category-new.component.css'],
+  providers : [UserService]
 })
 export class CategoryNewComponent implements OnInit {
 
-  constructor() { }
+  public pagetitle :string;
+  public identity;
+  public token;
+  public category : Category;
+
+  constructor(
+    private _route : ActivatedRoute,
+    private _router : Router,
+    private _userService : UserService
+  ) {
+    this.pagetitle = 'Nueva categoria';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.category = new Category(1,'');
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.category);
   }
 
 }
